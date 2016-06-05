500f1dafb731bdf8a125eb174483676324230c09
Roman Zaycev <box@romanzaycev.ru>
2016-06-06 00:32:59 +0600

    Add Event Listener tests

---
+++ tests/Events/ListenerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Virmire\Events\Listener;
+
+class ListenerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testListenerConstruct()
+    {
+        $foo = new Listener(function (){});
+        $this->assertInstanceOf(Listener::class, $foo);
+    }
+    
+    public function testListenerWithWrongCallback()
+    {
+        $this->expectException(\TypeError::class);
+        $foo = new Listener('');
+    }
+
+}
