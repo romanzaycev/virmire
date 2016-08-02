536ebc471adec079469e4ae891056fc4aa146494
Roman Zaycev <box@romanzaycev.ru>
2016-08-03 01:52:59 +0600

    Http\Response finalization

--- virmire/Http/Response.php
+++ virmire/Http/Response.php
@@ -9,25 +9,120 @@ namespace Virmire\Http;
  */
 class Response
 {
-    
+
     /**
-     * Response constructor.
+     * @var int
      */
-    public function __construct()
-    {
-        
-    }
-    
-    /**
-     * @var string
-     */
-    protected $body = '';
-    
+    private $statusCode = 200;
+
     /**
      * @var array
      */
-    protected $headers = [];
-    
+    private $headers = [
+        'Content-Type' => 'text/html; charset=UTF-8'
+    ];
+
+    /**
+     * @var string
+     */
+    private $body = '';
+
+    /**
+     * @var bool
+     */
+    private $isRedirect = false;
+
+    /**
+     * @param int $code
+     *
+     * @return Response
+     * @throws HttpException
+     */
+    public function setStatusCode(int $code) : Response
+    {
+        if ((100 > $code) || (599 < $code)) {
+            throw new HttpException(
+                sprintf('Invalid HTTP response code, %u', $code)
+            );
+        }
+
+        $this->isRedirect = (300 <= $code) && (307 >= $code);
+        $this->statusCode = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRedirect() : bool
+    {
+        return $this->isRedirect;
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     *
+     * @return Response
+     */
+    public function setHeader(string $name, string $value) : Response
+    {
+        $this->headers[$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders() : array
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @return Response
+     */
+    public function writeHeaders() : Response
+    {
+        if(!headers_sent()){
+            $statusCodeSent = false;
+
+            if (count($this->headers) == 0) {
+                return $this;
+            }
+
+            foreach ($this->headers as $headerName => $headerValue) {
+                if (!$statusCodeSent) {
+                    header(
+                        sprintf('%s: %s', $headerName, $headerValue),
+                        false,
+                        $this->statusCode
+                    );
+
+                    $statusCodeSent = true;
+                } else {
+                    header(
+                        sprintf('%s: %s', $headerName, $headerValue)
+                    );
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Response
+     */
+    public function writeBody() : Response
+    {
+        echo $this->body;
+
+        return $this;
+    }
+
     /**
      * @param string $body
      */
@@ -35,7 +130,7 @@ class Response
     {
         $this->body = $body;
     }
-    
+
     /**
      * @return string
      */
@@ -43,15 +138,13 @@ class Response
     {
         return $this->body;
     }
-    
-    public function writeHeaders()
+
+    /**
+     * @return void
+     */
+    public function sendResponse()
     {
-        
+        $this->writeHeaders()->writeBody();
     }
-    
-    public function writeBody()
-    {
-        
-    }
-    
+
 }
