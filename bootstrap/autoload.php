2dbda384f82300a954668b132fa44d1aafffd6d5
Roman Zaycev <box@romanzaycev.ru>
2017-02-11 18:30:42 +0700

    Remove autoloader unnecessary calls

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -5,10 +5,3 @@ require __DIR__ . '/../virmire/Autoloader.php';
 $autoloader = Virmire\Autoloader::getInstance();
 
 $autoloader->addNamespace('Virmire', __DIR__ . '/../virmire');
-$autoloader->addNamespace('Virmire\\Interfaces', __DIR__ . '/../virmire/Interfaces');
-$autoloader->addNamespace('Virmire\\Traits', __DIR__ . '/../virmire/Traits');
-$autoloader->addNamespace('Virmire\\Exceptions', __DIR__ . '/../virmire/Exceptions');
-$autoloader->addNamespace('Virmire\\Collections', __DIR__ . '/../virmire/Collections');
-$autoloader->addNamespace('Virmire\\Http', __DIR__ . '/../virmire/Http');
-$autoloader->addNamespace('Virmire\\Http\\Exceptions', __DIR__ . '/../virmire/Http/Exceptions');
-$autoloader->addNamespace('Virmire\\Events', __DIR__ . '/../virmire/Events');
